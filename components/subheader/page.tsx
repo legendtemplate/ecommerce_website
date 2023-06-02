33790d077c0023435eb329a161b01afeb357f078
528982tts <mmuzammiloff@gmail.com>
2023-06-02 10:06:54 +0500

    updated : 09:59 02/06/2023

--- components/subheader/page.tsx
+++ components/subheader/page.tsx
@@ -1,23 +1,39 @@
-
-
-
-
+"use client";
 import React from "react";
-
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaBars } from "react-icons/fa";
-
+import Container from "../container/page";
 export default function page() {
+   const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
     <>
       <div className="bg-gray-100 shadow-lg">
-        <div className="container mx-auto px-6 flex gap-8 items-center ">
-          <div>
-            <button className="bg-blue-500 text-white px-6 py-3 flex items-center"> <span className="mr-3"><FaBars /> </span> All Categeories</button>
+        <Container>
+        <div className="flex gap-8 items-center ">
+          <div className="w-full">
+            <button className="bg-blue-500 text-white px-6 py-3 lg:text-base md:text-xs sm:text-base text-xs w-full flex items-center" onClick={handleOpen}> <span className="mr-3"><FaBars /> </span> All Categeories</button>
+            
+            {open ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button>Menu 1</button>
+          </li>
+          <li className="menu-item">
+            <button>Menu 2</button>
+          </li>
+        </ul>
+      ) : null}
           </div>
           <div className="link">
-            <ul className="flex gap-6 relative">
+            
+              <button className="md:hidden block bg-blue-400 pr-3 pl-3 text-white h-10 w-10"><FaBars /></button>
+            <ul className="md:flex gap-6 relative hidden">
               <li>
                 <a href="">Home</a>
               </li>
@@ -47,7 +63,8 @@ export default function page() {
             </ul>
           </div>
         </div>
-      </div>
+      </Container>
+        </div>
     </>
   );
 }
